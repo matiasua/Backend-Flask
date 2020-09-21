e16926c51ab9ba5af2edb7525635114d7704c5f8
matiasua <matiasncp7@gmail.com>
2020-09-21 00:49:57 -0300

    se creo db en mongo y flask, se crearon API POST, GET Users, user, DELETE y PUT, se inicializo el projecto flask y configuro todo a la espera de conexion con backend

--- src/app.py
+++ src/app.py
@@ -1,8 +1,70 @@
-from flask import Flask
-from flask_pymongo import PyMongo
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo, ObjectId
 from flask_cors import CORS
 
 # Se inicializa flask y retorna instancia llamada app
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost/pythonreactdb"
-PyMongo(app)
+mongo = PyMongo(app)
+CORS(app)
+
+# cada vez que llame a db hago referencia a db.users (coleccion de users)
+db = mongo.db.users
+
+
+@app.route('/users', methods=['POST'])
+def createUser():
+    id = db.insert({
+      'name': request.json['name'],
+      'email': request.json['email'],
+      'password': request.json['password']
+    })
+    return jsonify(str(ObjectId(id)))
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+    # La lista de usuarios le declaro una variable (users=[]) 
+    users = []
+    for doc in db.find():
+        users.append({
+            '_id': str(ObjectId(doc['_id'])),
+            'name': doc['name'],
+            'email': doc['email'],
+            'password': doc['password']
+        })
+    return jsonify(users)
+
+
+@app.route('/users/<id>', methods=['GET'])
+def getUser(id):
+    user = db.find_one({'_id': ObjectId(id)})
+
+    return jsonify({
+        '_id': str(ObjectId(user['_id'])),
+        'name': user['name'],
+        'email': user['email'],
+        'password': user['password']
+    })
+
+@app.route('/users/<id>', methods=['DELETE'])
+def deleteUser(id):
+    db.delete_one({'_id': ObjectId(id)})
+    return jsonify({'mensaje': 'User Deleted'})
+
+@app.route('/users/<id>', methods=['PUT'])
+def updateUser(id):
+    db.update_one({'_id': ObjectId(id)}, {'$set': {
+      'name': request.json['name'],
+      'email': request.json['email'],
+      'password': request.json['password']
+    }})
+    return jsonify({'mensaje': 'User Updated'})
+
+
+
+#inicializando
+if __name__ == "__main__":
+  app.run(debug=True)
+
+
+
